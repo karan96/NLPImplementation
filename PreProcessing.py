4389bc19436b61f9291327625464275717c6a49c
karan96 <saxenak96@gmail.com>
2021-05-13 21:58:47 +0530

    Update: Modifications made as per Supervisor's review

--- PreProcessing.py
+++ PreProcessing.py
@@ -3,7 +3,7 @@ import pandas as pd
 from time import time
 import spacy
 
-def cleaning(doc):
+def clean(doc):
     # Lemmatizes and removes stopwords
     # doc needs to be a spacy Doc object
     txt = [token.lemma_ for token in doc if not token.is_stop]
@@ -13,13 +13,13 @@ def cleaning(doc):
     if len(txt) > 2:
         return ' '.join(txt)
 
-def first():
-    df = pd.read_csv('D:\Desktop\Papers\Data\simpsons_dataset.csv')
+def load_corpus(path_to_file):
+    df = pd.read_csv(path_to_file)
     df = df.dropna().reset_index(drop=True)
     nlp = spacy.load('en_core_web_sm', disable=['ner', 'parser']) # disabling Named Entity Recognition for speed
     brief_cleaning = (re.sub("[^A-Za-z']+", ' ', str(row)).lower() for row in df['spoken_words'])
     t = time()
-    txt = [cleaning(doc) for doc in nlp.pipe(brief_cleaning, batch_size=5000)]
+    txt = [clean(doc) for doc in nlp.pipe(brief_cleaning, batch_size=5000)]
     print('Time to clean up everything: {} mins'.format(round((time() - t) / 60, 2)))
     df_clean = pd.DataFrame({'clean': txt})
     df_clean = df_clean.dropna().drop_duplicates()
--- main.py
+++ main.py
@@ -1,30 +1,79 @@
 import pandas as pd
 import PreProcessing as Pre
-import GensimFunc as GenF
-import ExtractWords as EW
+import w2v_train as GenF
+import w2v_similarity as EW
 import GoldStandardProc as GS
 import SecondCorpus as SC
+import os
+import nltk
+from nltk.corpus import brown
 
-path_to_file = 'D:\Desktop\Papers\w2v_project\'
+nltk.download('brown')
 
-simFile = path_to_file + "wordsim_similarity_goldstandard.txt"
+# setting directory path
+# This File assumes that the files related to project are stored in Evaluation Folder.
 
-wordsim_df = pd.read_csv(simFile,sep='\t',header=None)
+data_file = "simpsons_dataset.csv"
 
-wordsim_df.columns = ['Animal_1','Animal_2','Sim_Score']
+simFile = "wordsim_similarity_goldstandard.txt"
+
+# setting up window size range and dimensionality range
+
+cores = multiprocessing.cpu_count()
+w = list(range(2, 11))
+d = list(range(10, 100, 10)) + list(range(100, 600, 100))
+
+work_dir = os.path.abspath(__file__)
+
+dir_name = os.path.dirname(__file__)
+
+os.chdir(dir_name)
+
+cwd = os.getcwd()
 
 
-df_clean = Pre.first()
+def find(simFile, eval_path):
+    for root, dirs, files in os.walk(eval_path):
+        if simFile in files:
+            print("File Found")
+        else:
+            print("WordSim Similarity Goldstandard file not found.")
+            print(
+                "Please download it from http://alfonseca.org/pubs/ws353simrel.tar.gz.")
+            print("Unzip wordsim_similarity_goldstandard into the evaluation folder ")
+            print("Exiting Program")
+            exit()
+
+
+if os.path.exists('evaluation'):
+    print("Path Exists Looking for Wordsim Similarity File")
+    find(simFile, eval_path)
+else:
+    os.mkdir(dir_name + '/evaluation')
+
+eval_path = cwd + '\evaluation'
+
+os.chdir(eval_path)
+
+find(simFile, eval_path)
+
+path_to_file = os.path.join(eval_path, data_file)
+
+# Pre Processing and Training
+
+df_clean = Pre.load_corpus(path_to_file)
 
 sentences = GenF.Phraser(df_clean)
 
-model_list = GenF.build_model(path_to_file, sentences)
+model_list = GenF.build_model(path_to_file, w, d, cores, sentences, eval_path)
 
-EW.cosine(path_to_file)
+EW.calculate_sim(eval_path, w, d, "homer", sim_name='cos')
+EW.calculate_sim(eval_path, w, d, "homer", sim_name='man')
+EW.calculate_sim(eval_path, w, d, "homer", sim_name='euc')
 
-EW.euc(path_to_file)
-
-EW.manh(path_to_file)
+'''We do not need to calculate map and ndcg values for each metric? But we did calculate it while we were working on 
+the project. I will be uploading another piece of reusable code to remove the usage of same code three times for 
+calculating map and ndcg scores. '''
 
 EW.map_cos()
 
@@ -38,13 +87,19 @@ EW.ndcg_euc()
 
 EW.ndcg_manh()
 
-word_dic = GS.preprocess(path_to_file)
+word_dic = GS.preprocess(eval_path)
 
-GS.missingWords(path_to_file)
+GS.missingWords(word_dic, f'w2v_model_window-2_size-10.mdl')
 
-GS.goldstand_map(path_to_file)
+avg_dict = GS.goldstand_map(eval_path)
+plt.figure(figsize=(30, 20))
+names = list(avg_dict.keys())
+values = list(avg_dict.values())
+plt.plot(names, values, color='b')
+plt.xticks(rotation=90, fontsize='small')
+plt.title("Cosine Similarity Metric for Golden Standard vs Trained Models: MAP")
+plt.show()
 
-SC.modeltrain(path_to_file)
+model_list = GenF.build_model(path_to_file, w, d, cores, sentences, eval_path, new_corpus=1)
 
-SC.compare_map(word_dic, path_to_file)
-
+SC.compare_map(model_list, eval_path)
