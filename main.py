36e709736185d8d0f6c820befe1705fb4124e6f0
karan96 <saxenak96@gmail.com>
2021-05-27 01:05:59 +0530

    Update 3: Changes made as per Mentor's Comments

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-import pandas as pd
-import PreProcessing as Pre
-import w2v_train as GenF
-import w2v_similarity as EW
-import GoldStandardProc as GS
-import SecondCorpus as SC
 import os
 import nltk
+import multiprocessing
+
+import logging  # Setting up the logging to monitor gensim
+import PreProcessing as Pre
+import w2v_train as genf
+import w2v_similarity as EW
+import GoldStandardProc as GS
+import visualization as vs
 from nltk.corpus import brown
 
 nltk.download('brown')
 
-# setting directory path
+# Setting directory path
 # This File assumes that the files related to project are stored in Evaluation Folder.
 
 data_file = "simpsons_dataset.csv"
-
 simFile = "wordsim_similarity_goldstandard.txt"
+simpsons_data = "./output/simpsons/"
 
 # setting up window size range and dimensionality range
 
@@ -23,19 +25,39 @@ cores = multiprocessing.cpu_count()
 w = list(range(2, 11))
 d = list(range(10, 100, 10)) + list(range(100, 600, 100))
 
-work_dir = os.path.abspath(__file__)
-
+# Setting up Directory Paths
 dir_name = os.path.dirname(__file__)
 
 os.chdir(dir_name)
 
 cwd = os.getcwd()
+eval_path = cwd
+
+log_path = f'{eval_path}\\output\\logs\\'
+full_path = f'{eval_path}\\data\\raw\\'
+output_path = f'{eval_path}\\output\\'
+brown_path = f'{eval_path}\\output\\brown\\'
+evaluation_charts = f'{eval_path}\\evaluation\\charts\\'
+
+if os.path.exists(evaluation_charts):
+    os.chdir(evaluation_charts)
+else:
+    os.makedirs(evaluation_charts)
+
+if os.path.exists(evaluation_charts):
+    os.chdir(evaluation_charts)
+else:
+    os.makedirs(log_path)
+
+logging.basicConfig(format="%(levelname)s - %(asctime)s: %(message)s", datefmt='%H:%M:%S', level=logging.INFO,
+                    filename=log_path)
 
 
-def find(simFile, eval_path):
-    for root, dirs, files in os.walk(eval_path):
-        if simFile in files:
+def find(simfile, file_path):
+    for root, dirs, files in os.walk(file_path):
+        if simfile in files:
             print("File Found")
+            exit()
         else:
             print("WordSim Similarity Goldstandard file not found.")
             print(
@@ -45,61 +67,77 @@ def find(simFile, eval_path):
             exit()
 
 
+if os.path.exists(full_path):
+    os.chdir(full_path)
+else:
+    os.makedirs(full_path)
+
+if os.path.exists(brown_path):
+    os.chdir(log_path)
+else:
+    os.makedirs(brown_path)
+
 if os.path.exists('evaluation'):
     print("Path Exists Looking for Wordsim Similarity File")
-    find(simFile, eval_path)
+    sim_file = os.path.join(eval_path, 'evaluation')
+    print(sim_file)
+    find(simFile, sim_file)
 else:
-    os.mkdir(dir_name + '/evaluation')
+    os.makedirs('evaluation')
+    sim_file = os.path.join(eval_path, 'evaluation')
+    print(sim_file)
+    find(simFile, sim_file)
 
-eval_path = cwd + '\evaluation'
+find(simFile, full_path)
 
-os.chdir(eval_path)
-
-find(simFile, eval_path)
-
-path_to_file = os.path.join(eval_path, data_file)
+path_to_file = os.path.join(full_path, data_file)
 
 # Pre Processing and Training
+distance = ['cos', 'man', 'euc']
+metrics = ['map', 'ndcg']
+
+true_dict = {'Homer': {'salad': 10,
+                  'God': 9,
+                  'fat': 8,
+                  'Married': 7,
+                  'far': 6,
+                  'moron': 5,
+                  'wife': 4,
+                  'medical': 3,
+                  'extra': 2,
+                  'dammit': 1},
+            'Marge': {
+                'wife': 10,
+                  'malibu_stacy': 9,
+                  'picture': 8,
+                  'sweety': 7,
+                  'hardly': 6,
+                  'carry': 5,
+                  'chair': 4,
+                  'look': 3,
+                  'board': 2,
+                  'probably': 1
+            }}
 
 df_clean = Pre.load_corpus(path_to_file)
+sentences = genf.phraser_train(df_clean)
+model_list = genf.build_model(w, d, cores, sentences, output_path)
+pytrec_dict, filename = EW.calculate_sim(output_path, w, d, true_dict, metrics, "homer", sim_name='cos')
+vs.visualize(pytrec_dict, evaluation_charts, filename)
+pytrec_dict = EW.calculate_sim(output_path, w, d, true_dict, metrics, "homer", sim_name='man')
+vs.visualize(pytrec_dict, evaluation_charts, filename)
+pytrec_dict = EW.calculate_sim(output_path, w, d, true_dict, metrics, "homer", sim_name='euc')
+vs.visualize(pytrec_dict, evaluation_charts, filename)
 
-sentences = GenF.Phraser(df_clean)
+brown_sents = brown.sents(categories='news')
+model_list_brown = genf.build_model(w, d, cores, brown_sents, brown_path)
 
-model_list = GenF.build_model(path_to_file, w, d, cores, sentences, eval_path)
+word_dic = GS.process(f'{brown_path}{simFile}')
 
-EW.calculate_sim(eval_path, w, d, "homer", sim_name='cos')
-EW.calculate_sim(eval_path, w, d, "homer", sim_name='man')
-EW.calculate_sim(eval_path, w, d, "homer", sim_name='euc')
+# Up to Here
+# Print Number of missing words from our Corpus when compared with GoldStandard
+GS.missingWords(word_dic, f'{output_path}w2v_model_window-2_size-10.mdl')
 
-'''We do not need to calculate map and ndcg values for each metric? But we did calculate it while we were working on 
-the project. I will be uploading another piece of reusable code to remove the usage of same code three times for 
-calculating map and ndcg scores. '''
 
-EW.map_cos()
-
-EW.map_euc()
-
-EW.map_man()
-
-EW.ndcg_cos()
-
-EW.ndcg_euc()
-
-EW.ndcg_manh()
-
-word_dic = GS.preprocess(eval_path)
-
-GS.missingWords(word_dic, f'w2v_model_window-2_size-10.mdl')
-
-avg_dict = GS.goldstand_map(eval_path)
-plt.figure(figsize=(30, 20))
-names = list(avg_dict.keys())
-values = list(avg_dict.values())
-plt.plot(names, values, color='b')
-plt.xticks(rotation=90, fontsize='small')
-plt.title("Cosine Similarity Metric for Golden Standard vs Trained Models: MAP")
-plt.show()
-
-model_list = GenF.build_model(path_to_file, w, d, cores, sentences, eval_path, new_corpus=1)
-
-SC.compare_map(model_list, eval_path)
+pytrec_dict = EW.evaluate(output_path, w, d, distance, metrics, './evaluation/results/', word_dic)
+vs.visualize(pytrec_dict, evaluation_charts, 'Comparison Against WordSim GoldStandard')
